36c4f18e983b0818f182d3d39a07a366c108a7cf
Anthony <haddad-anthony@live.com>
2020-02-05 11:04:41 +0200

    Responsive navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import EventCard from './components/UpComingEvents/EventCard/EventCard'
-function App() {
-  return (
-    <div>
-      <EventCard />
-=======
 import Header from './components/Header/Header';
+import EventCard from './components/UpComingEvents/EventCard/EventCard';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
->>>>>>> 544da7eff648653c67e98facdf771e6fbfc35c9f
+    <div>
+      <Header />
+      <Footer />
     </div>
   );
 }
--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import './Header.css';
 
 class Header extends Component {
+  state={
+    hide:false,
+    status: "header_navlist"
+  }
+
+  handleClick = ()=>{
+    this.setState({ hide: !this.state.hide });
+    console.log(this.state.hide);
+    if(this.state.hide) {
+      this.setState({status: "header_navlist"});
+    } else {
+      this.setState({status: "header_navlist hide"});
+    }
+  }
+
+
   render() {
     return (
       <header>
+        <div onClick={this.handleClick} className="burger">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
         <nav>
           <div id="hero">
             <h1>MESHWAR</h1>
           </div>
-          <ul className="header_navlist">
+          <ul className={this.state.status}>  
             <li><a className="header_navlink" href="#">Events</a></li>
             <li><a className="header_navlink" href="#">Gallery</a></li>
             <li><a className="header_navlink" href="#">Contact Us</a></li>
           </ul>
         </nav>
-      </header>
+      </header> 
     );
   }
 }
