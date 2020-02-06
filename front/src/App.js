5d521ab6ef01ff8fcd8a7ea57fb689c1ddfb3ef9
Anthony <haddad-anthony@live.com>
2020-02-06 12:53:30 +0200

    wrapping things up

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-<<<<<<< HEAD
-import GalleryContainer from './components/GalleryContainer/GalleryContainer';
-
-=======
-import Slider from './components/Slider/Slider';
-import EventCard from './components/UpComingEvents/EventCard/EventCard';
-import Footer from './components/Footer/Footer';
 import Home from './pages/HomePage/HomePage';
->>>>>>> 8e73e72f3a480d2596e3737742051ba2e3533a7a
+import Header from './components/Header/Header';
+import GalleryContainer from './components/GalleryContainer/GalleryContainer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header/>
-      <HowItWorks />
-      <GalleryContainer />
-=======
       <Home />
->>>>>>> 8e73e72f3a480d2596e3737742051ba2e3533a7a
-      
     </div>
   );
 }
