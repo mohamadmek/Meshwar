b7056157545ae10a7b9b03a18629da303a41ccff
Anthony <haddad-anthony@live.com>
2020-02-06 14:00:31 +0200

    events page done

--- front/src/App.js
+++ front/src/App.js
@@ -4,11 +4,12 @@ import Home from './pages/HomePage/HomePage';
 import Header from './components/Header/Header';
 import GalleryContainer from './components/GalleryContainer/GalleryContainer';
 import Footer from './components/Footer/Footer';
+import EventsPage from './pages/EventsPage/EventsPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <EventsPage />
     </div>
   );
 }
