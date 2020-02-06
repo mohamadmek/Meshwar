0e7a79c97472441773902ec9915412e44a318c83
Anthony <haddad-anthony@live.com>
2020-02-06 12:45:24 +0200

    u

--- front/src/pages/HomePage/HomePage.js
+++ front/src/pages/HomePage/HomePage.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Slider from '../../components/Slider/Slider';
-
+import Form from '../../components/Form/Form';
 class HomePage extends Component {
   render() {
     return (
       <main className="wrapper">
         <Header />
-        <Slider />
-        <Footer />
+        <Form />
       </main>
     );
   }
