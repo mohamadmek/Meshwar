637f3bea30224ae595069308d40c6f73886d7a3a
Anthony <haddad-anthony@live.com>
2020-02-18 15:18:04 +0200

    pulling from upstream

--- front/src/App.js
+++ front/src/App.js
@@ -8,76 +8,14 @@ import Gallery from './pages/GalleryPage/GalleryPage'
 import ContactPage from './pages/ContactUsPage/ContactUsPage'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import roccaMarina from './assets/images/rocca-marina.jpg';
-import faraya from './assets/images/faraya.jpg';
-import roadOfDeath from './assets/images/road-of-death.jpg';
-import tripoli from './assets/images/tripoli.jpg';
-import camping from './assets/images/camping.jpg';
-import bbq from './assets/images/bbq.jpg';
 
 class App extends React.Component {
    state = {
-    //events: [
-    //  {
-    //    title: "Road Of Death",
-    //    location: "Tripoli | Bcharre | Cedars of God",
-    //    date: "Feb 9, 2020 | 8AM - 6PM",
-    //    price: "$25",
-    //    remainingSeats: 10,
-    //    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam asperiores laboriosam quidem harum iusto saepe.',
-    //    imageSource: roadOfDeath,
-    //  },
-    //  {
-    //    title: "Faraya",
-    //    location: "Jeita | Aajaltoun | Faraya",
-    //    date: "Feb 12, 2020 | 8AM - 6PM",
-    //    price: "$20",
-    //    remainingSeats: 15,
-    //    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia accusantium deleniti suscipit nobis sunt? Fuga reiciendis nisi velit quia?',
-    //    imageSource: faraya,
-    //  },
-    //  {
-    //    title: "Tripoli",
-    //    location: "Batroun | Chekka | Tripoli",
-    //    date: "March 16, 2020 | 8AM - 6PM",
-    //    price: "$22",
-    //    remainingSeats: 18,
-    //    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia accusantium deleniti suscipit nobis sunt? Fuga reiciendis nisi velit quia?',
-    //    imageSource: tripoli,
-    //  },
-    //  {
-    //    title: "Camping by the fire",
-    //    location: "Byblos Castle | Blat",
-    //    date: "June 22, 2020 | 8AM - 6PM",
-    //    price: "$30",
-    //    remainingSeats: 5,
-    //    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia accusantium deleniti suscipit nobis sunt? Fuga reiciendis nisi velit quia?',
-    //    imageSource: camping,
-    //  },
-    //  {
-    //    title: "Fun in the Sun",
-    //    location: "Rocca Marina",
-    //    date: "July 28, 2020 | 12AM - 6PM",
-    //    price: "$25",
-    //    remainingSeats: 11,
-    //    description: '',
-    //    imageSource: roccaMarina,
-    //  },
-    //  {
-    //    title: "Bbq Fest",
-    //    location: "Chahtoul Valley",
-    //    date: "July 28, 2020 | 11AM - 11PM",
-    //    price: "$40",
-    //    remainingSeats: 16,
-    //    description: 'Some random stuff',
-    //    imageSource: bbq,
-    //  }
-    //],
-    gallery: [faraya, tripoli, roccaMarina, roadOfDeath, camping, bbq]
+
   }
 
    
-    async componentDidMount(){
+  async componentDidMount(){
 	const url = 'http://localhost:8080/events';
 	const response = await fetch(url, {
 	    method: 'GET',
--- front/src/components/MiniGallery/MiniGallery.js
+++ front/src/components/MiniGallery/MiniGallery.js
@@ -2,24 +2,24 @@ import React, {Component} from 'react';
 import './MiniGallery.css'
 import Mini from '../Mini/Mini'
 class MiniGallery extends Component{
-      randNB = () => {
-       const arrRand = []
-       while (arrRand.length < 6){ 
-	 let rand = Math.floor(Math.random()*this.props.src.length)
-	 if(arrRand.includes(rand)){
-	   rand = Math.floor(Math.random()*this.props.src.length)
-	 } else {
-	   arrRand.push(rand)
-	 }
-       }
-       // console.log(arrRand)
-       // // return(<Mini src={this.props.src[2]}/>)
-       //   arrRand.forEach(index => {
-       //     console.log(index);
-       //     return <Mini src={this.props.src[1]} />
-       // })
-       return arrRand;
-     }
+  //     randNB = () => {
+  //      const arrRand = []
+  //      while (arrRand.length < 6){ 
+	//  let rand = Math.floor(Math.random()*this.props.src.length)
+	//  if(arrRand.includes(rand)){
+	//    rand = Math.floor(Math.random()*this.props.src.length)
+	//  } else {
+	//    arrRand.push(rand)
+	//  }
+  //      }
+  //      // console.log(arrRand)
+  //      // // return(<Mini src={this.props.src[2]}/>)
+  //      //   arrRand.forEach(index => {
+  //      //     console.log(index);
+  //      //     return <Mini src={this.props.src[1]} />
+  //      // })
+  //      return arrRand;
+  //    }
 
   // looping = () => {
   //   for(let i=0; i<this.props.src.length; i++) {
@@ -28,15 +28,15 @@ class MiniGallery extends Component{
   // }
   
     render(){
-      const random = this.randNB();
+      // const random = this.randNB();
       return (
 	  <div>
-	  <section className="MiniGallery_section">
+	  {/* <section className="MiniGallery_section">
 	       {random.map(index => {
 		return <Mini src={this.props.src[index]} />
 	       })}
 	       
-	      </section>  
+	      </section>   */}
 	  </div>
       )
     }
